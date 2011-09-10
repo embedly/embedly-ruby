32c2d99dc093f41d537d23884b7699b51b8f71c2
Felipe Elias Philipp <felipeelias@gmail.com>
2011-09-10 23:46:13 +0200

    Add configuration class
    This class is going to be a replacement to the old OpenStruct. This way
    we can avoid typos and invalid settings.
    
    It also allows more complex kinds of customizations.

---
+++ lib/embedly/configuration.rb
@@ -0,0 +1,39 @@
+class Embedly::Configuration
+  attr_accessor :key
+
+  def initialize
+    self.reset
+  end
+
+  def debug?
+    self.logger.debug?
+  end
+
+  def debug=(true_or_false)
+    set_logger_level(true_or_false)
+  end
+
+  def logger
+    @logger ||= default_logger
+  end
+
+  def logger=(log)
+    @logger = log
+    set_logger_level(self.debug?)
+  end
+
+  def reset
+    self.logger = default_logger
+    self.debug  = false
+  end
+
+  private
+
+  def default_logger
+    Logger.new(STDERR)
+  end
+
+  def set_logger_level(true_or_false)
+    logger.level = true_or_false ? Logger::DEBUG : Logger::ERROR
+  end
+end
