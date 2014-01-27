f31868298efd1c32d2e8195cb5a528b16cdc581f
Bob Corsaro <bob@embed.ly>
2014-01-27 11:47:32 -0500

    Don't require faraday

--- lib/embedly/request/faraday.rb
+++ lib/embedly/request/faraday.rb
@@ -1,22 +1,25 @@
-require 'faraday'
+begin
+  require 'faraday'
 
-# Patch Faraday::Response to provide a status code
-module Faraday
-  class Response
-    alias_method :code, :status unless method_defined?(:code)
+  # Patch Faraday::Response to provide a status code
+  module Faraday
+    class Response
+      alias_method :code, :status unless method_defined?(:code)
+    end
   end
-end
 
-module Embedly
-  module Request
-    class Faraday < Base
-      def get(uri, options = {})
-        ::Faraday.get(uri.to_s, nil, options[:headers])
+  module Embedly
+    module Request
+      class Faraday < Base
+        def get(uri, options = {})
+          ::Faraday.get(uri.to_s, nil, options[:headers])
+        end
       end
     end
   end
-end
 
-Embedly.configuration.add_requester :faraday do |api|
-  Embedly::Request::Faraday.new(api)
+  Embedly.configuration.add_requester :faraday do |api|
+    Embedly::Request::Faraday.new(api)
+  end
+rescue LoadError
 end
